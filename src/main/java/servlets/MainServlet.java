d76a7be0f05b8aa1d765a3614e55cc15365bd575
ntluan95 <ntluandt95@gmail.com>
2022-01-28 13:18:50 -0600

    initial commit

---
+++ src/main/java/servlets/MainServlet.java
@@ -0,0 +1,19 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse respone) throws ServletException, IOException {
+		respone.getWriter().append("hello");
+	}
+	
+	
+
+}
